67a2fcfa6a7e3effb1659056c177369f437e0136
luozhen <luozhen@gaodun.com>
2017-04-28 18:04:00 +0800

    edit model

--- app/Model/BaseModel.php
+++ app/Model/BaseModel.php
@@ -28,38 +28,50 @@ class BaseModel extends Model
         //return app('db')->table($this->table)->lists('sortname');
        // $db = $this->model;
         
-                        $this->model->where(
-                            function($query){
-                                $query->where('sid','<','61');
-                                $query->where('pid','=','0');
-                            }
-                        )
-                        ->orwhere('pid', 1);
-                       $this->model->orderBy('sid','desc');
-                        $this->model->orderBy('pid','asc');
+                    //     $this->model->where(
+                    //         function($query){
+                    //             $query->where('sid','<','61');
+                    //             $query->where('pid','=','0');
+                    //         }
+                    //     )
+                    //     ->orwhere('pid', 1);
+                    //    $this->model->orderBy('sid','desc');
+                    //     $this->model->orderBy('pid','asc');
                        // ->orderby(['sid','pid'],['desc','asc'])
                     //    ->orderBy(function($order){
                     //        $order->orderby('sid','asc');
                     //    })
                    // ->orderby([['column' => 'sid', 'direction' => 'desc'],['column' => 'pid', 'direction' => 'asc']])
-                        $this->model->get();
+                        //$this->model->get();
  
 
 
  
     // 获取已执行的查询数组
-   DB::table($this->table)->where(['sid'=>1,'pid'=>0])->orderBy('sid','asc')->get();
-
+    //DB::table($this->table)->where(['sid'=>1,'pid'=>0])->orderBy('sid','asc')->offset(2)->limit('1')->get();
+    $where = ['sid'=>[1,'>'],'pid'=>1];
+    
+    $orderby = ['sid'=>'desc','pid'=>'asc'];
+    //$this->orderby($orderby);
+     //$this->model->orderBy('sid','desc');
+     //$this->model->orderBy('pid','asc');
+      $return = $this->where($where)->orderby($orderby)->limit(5)->get();
+    // $return = $this->model->get();
+    // DB::table($this->table)->$this->where(['sid'=>[1,'>'],'pid'=>0]);
+      //DB::table($this->table)->get();
+      
     $log = DB::getQueryLog();
    dd($log);   //打印sql语句
  
+ return $return;
 
- exit;
+ //exit;
 
 
         //return app('db')->table($this->table)->value($field)->get();
     }
 
+    
     /**
      * 通过条件获取多条数据
      *
@@ -67,7 +79,7 @@ class BaseModel extends Model
      */
     public function  fetchAll()
     {
-
+        
     }
 
     /**
@@ -110,29 +122,45 @@ class BaseModel extends Model
 
     }
 
-    /**
-    * where条件组装
-    *
-    * @return void
-    */
-   private function where($where)
+ 
+
+    //基础方法改写
+    public function where($where = [])
+    {
+        foreach($where as $key=>$item){
+            if(is_array($item)){
+                $this->model->where($key,$item[1],$item[0]);
+            } else {
+                $this->model->where($key, $item);
+            }
+            
+        }
+        return $this;
+    }
+
+    public function orderby($orderby = []){
+        foreach($orderby as $key=>$item){
+            $this->model->orderBy($key,$item);
+        }
+        return $this;
+    }
+
+    function limit($limit, $offset = 0)
+    {
+        $this->model->limit($limit)->offset($offset);
+        return $this;
+    }
+
+    public function get(){
+        return $this->model->get();
+    }
+   
+   public function one()
    {
-        // if(!empty($where)) {
-        //     foreach($where as $key=>$item){
-        //         if(is_array($item)){
-        //             app('db')->table($this->table)->where($key,$item[1], $item[0]);
-        //             continue;
-        //         }
-        //         app('db')->table($this->table)->where($key, $item);
-        //     }
-        // }
-
-
+       return $this->model->first();
    }
 
-
-    
-
-   
-
+   public function join(){
+        
+   }
 }	
